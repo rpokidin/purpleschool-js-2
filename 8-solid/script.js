7f76d55e7c04be6b0829425b2b68b35452775014
rpokidin <rpokidin@yandex.ru>
2025-09-17 14:50:11 +0300

    cmt 8-solid

---
+++ 8-solid/script.js
@@ -0,0 +1,59 @@
+
+class Billing {
+
+	amount
+
+	constructor(amount) {
+		this.amount = amount
+	}
+
+	calculateTotal() {}
+}
+
+class FixBilling extends Billing {
+
+	constructor(amount) {
+		super(amount)
+	}
+
+	calculateTotal() {
+		return this.amount
+	}
+}
+
+class HourBilling extends Billing {
+
+	constructor(amount, hors) {
+
+		super(amount)
+		this.hors = hors
+
+	}
+
+	calculateTotal() {
+
+		return this.amount * this.hors
+
+	}
+}
+
+class ItemBilling extends Billing {
+
+	constructor(amount, items) {
+
+		super(amount)
+		this.items = items
+        
+	}
+
+	calculateTotal() {
+		return this.amount * this.items
+	}
+}
+
+const fixBilling = new FixBilling(50)
+console.log(fixBilling.calculateTotal())
+const hourBilling = new HourBilling(10, 5)
+console.log(hourBilling.calculateTotal())
+const itemBilling = new ItemBilling(30, 20)
+console.log(itemBilling.calculateTotal())
