7daa053f31ee5da25857e532c944e57fe638775c
rpokidin <rpokidin@yandex.ru>
2025-09-17 15:56:42 +0300

    cmt 9-pokemon fix

--- 9-pokemon/script.js
+++ 9-pokemon/script.js
@@ -1,22 +1,21 @@
-
 const request = new XMLHttpRequest()
-request.open('GET', 'https://pokea2pi.co/api/v2/pokemon/ditto')
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto')
 request.send()
+
 request.addEventListener('load', function () {
+    const { abilities } = JSON.parse(this.responseText)
+    const abilityUrl = abilities[0].ability.url
+	console.log(abilities)
+	console.log(abilityUrl)
 
-	const { abilities } = JSON.parse(this.responseText)
-	const request = new XMLHttpRequest()
+    const abilityRequest = new XMLHttpRequest()
+    abilityRequest.open('GET', abilityUrl)
+    abilityRequest.send()
 
-	request.open('GET', `${abilities[1].ability.url}`)
-	request.send()
-	request.addEventListener('load', function () {
-
-		const { effect_entries } = JSON.parse(this.responseText)
-		console.log(effect_entries[1].effect)
-		
-	})
-
-})
-request.addEventListener('error', function () {
-	console.log(new Error('Ошибка при получении данных'))
+    abilityRequest.addEventListener('load', function () {
+        const abilityData = JSON.parse(this.responseText)
+        const description = abilityData.effect_entries[1].effect
+        console.log(abilityData)
+        console.log(description)
+    })
 })
